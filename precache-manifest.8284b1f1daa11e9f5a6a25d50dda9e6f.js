c533ec10ce0ae884452baf5293aee83299362cfd
Kuba Jaros <jarosk88@gmail.com>
2020-05-18 08:48:03 +0200

    Updates

---
+++ precache-manifest.8284b1f1daa11e9f5a6a25d50dda9e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3b9c936a052d46d0cd36ff3cac332a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "837ff53bcb9489a6913a",
+    "url": "/static/css/main.f407d192.chunk.css"
+  },
+  {
+    "revision": "766d93bd5f5f932485b3",
+    "url": "/static/js/2.38d3ac3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38d3ac3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837ff53bcb9489a6913a",
+    "url": "/static/js/main.7c314f14.chunk.js"
+  },
+  {
+    "revision": "955e57bad8c9e80d7c86",
+    "url": "/static/js/runtime-main.c1c61d22.js"
+  }
+]);
